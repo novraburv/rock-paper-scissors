f3d3de3a7745e15f7c32e060e6c70b3aaa10e30a
novraburv <78609440+novraburv@users.noreply.github.com>
2021-11-26 21:13:33 +0700

    create basic functions

---
+++ scripts/main.js
@@ -0,0 +1,43 @@
+function computerPlay() {
+    /* Randomly returns Rock, Paper, or Scissors */
+    let options = ["Rock", "Paper", "Scissors"];
+    const rand = Math.floor(Math.random() * 3);
+    return options[rand];
+}
+
+function playRound(playerSelection, computerSelection) {
+    /* 
+    Rolls a round of the game.
+    Compares player's selection with computers.
+    */
+   console.log(`player: ${playerSelection}, computer: ${computerSelection}`)
+    if (playerSelection === computerSelection) {
+        return 'tie';
+    } else if (
+        (playerSelection === "Rock" && computerSelection === "Scissors") ||
+        (playerSelection === "Paper" && computerSelection === "Rock") ||
+        (playerSelection === "Scissors" && computerSelection === "Paper")
+    ) {
+        return "win";
+    } else {
+        return "lose";
+    }
+}
+
+function game() {
+    let pcount = 0;
+    let cpucount = 0;
+    for (round = 0; round < 5; round++) {
+        let user = prompt("choose rock, paper, or scisors");
+        let result = playRound(user, computerPlay());
+        if (result === 'win') {
+            pcount++;
+        } else if (result === "lose") {
+            cpucount++;
+        } else {
+            pcount++;
+            cpucount++;
+        }
+    }
+    return `player: ${pcount}, computer: ${cpucount}`;
+}
